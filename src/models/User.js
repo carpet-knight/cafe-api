c46a4c53d4e9c8d6cab4ec9e2c4022270a54346a
Ivan Vasilev <vanokvas@gmail.com>
2021-02-23 23:30:07 +0300

    Add User model

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 127
+  },
+  login: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 2,
+    maxlength: 127
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 60,
+    maxlength: 60
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'manager', 'employee', 'customer']
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
