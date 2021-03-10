7522c65d88d91057613c553c8d5beab9fa6d224a
Ivan Vasilev <vanokvas@gmail.com>
2021-03-10 21:56:35 +0300

    Add validation schema for user creation

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -1,17 +1,27 @@
 const Joi = require('joi');
 
 
+const nameSchema = Joi.string().min(2).max(128).trim();
 const loginSchema = Joi.alternatives().try(
   Joi.string().min(2).max(128).token().trim(),
   Joi.string().email().lowercase().trim()
-).required();
-const passwordSchema = Joi.string().required().min(8).max(128).trim();
+);
+const passwordSchema = Joi.string().min(8).max(128).trim();
+const roleSchema = Joi.string().valid('admin', 'manager', 'employee', 'customer');
 
 const schemas = {
   loginSchema: Joi.object({
-    login: loginSchema,
-    password: passwordSchema
-  })
+    login: loginSchema.required(),
+    password: passwordSchema.required()
+  }),
+  user: {
+    creationSchema: Joi.object({
+      name: nameSchema.required(),
+      login: loginSchema.required(),
+      password: passwordSchema.required(),
+      role: roleSchema.required()
+    })
+  }
 }
 
 module.exports = schemas;
