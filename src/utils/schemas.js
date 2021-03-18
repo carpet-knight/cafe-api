5578803b29af4df7edae9119c0fd5f365c512695
Ivan Vasilev <vanokvas@gmail.com>
2021-03-18 18:37:21 +0300

    Add schema for database query validation

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -1,6 +1,8 @@
 const Joi = require('joi');
 
 
+/********************** schemas for user fields validation ************************/
+
 const nameSchema = Joi.string().min(2).max(128).trim();
 const loginSchema = Joi.alternatives().try(
   Joi.string().min(2).max(128).token().trim(),
@@ -8,6 +10,35 @@ const loginSchema = Joi.alternatives().try(
 );
 const passwordSchema = Joi.string().min(8).max(128).trim();
 const roleSchema = Joi.string().valid('admin', 'manager', 'employee', 'customer');
+
+/*********************************************************************************/
+
+/******************** schema for database query validation **********************/
+
+const querySchema = Joi.object({
+  limit: Joi.number().min(1),
+  offset: Joi.number().min(0),
+  fields: Joi.string(),
+  sort: Joi.string()
+}).pattern(
+  Joi.string(),
+  Joi.alternatives().try(
+    Joi.string().trim(),
+    Joi.object().pattern(
+      Joi.string().valid(
+        '$eq',
+        '$gt',
+        '$gte',
+        '$lt',
+        '$lte',
+        '$ne',
+      ),
+      Joi.string().trim()
+    ).min(1)
+  )
+);
+
+/*********************************************************************************/
 
 const schemas = {
   loginSchema: Joi.object({
@@ -21,7 +52,8 @@ const schemas = {
       password: passwordSchema.required(),
       role: roleSchema.required()
     })
-  }
+  },
+  querySchema
 }
 
 module.exports = schemas;
