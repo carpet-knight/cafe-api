cbd8fd5fd721abfa7b1fbdc79259abce698e68f5
Ivan Vasilev <vanokvas@gmail.com>
2021-03-19 23:06:12 +0300

    Add handlers for JSON parsing error & db query type casting error

--- src/middleware/errorHandlers.js
+++ src/middleware/errorHandlers.js
@@ -1,8 +1,11 @@
+const { CastError } = require('mongoose').Error;
+
 const {
   ClientError,
   ServerError,
   NotFoundError,
-  MethodNotAllowedError
+  BadRequestError,
+  MethodNotAllowedError,
 } = require('../utils/errors');
 
 
@@ -19,11 +22,19 @@ function basicHandler(err, req, res, next) {
     return res.status(err.status).json(err.toResponseBody());
   }
 
-  console.error(err);
-  const serverError = new ServerError(err);
-  return res
-    .status(serverError.status)
-    .json(serverError.toResponseBody());
+  if (err instanceof SyntaxError) {
+    // JSON parsing error handler
+    err = new BadRequestError('Failed to parse request body');
+  } else if (err instanceof CastError) {
+    // db query type casting error handler
+    err = new BadRequestError('Incorrect query parameter type');
+  } else {
+    // unknown error handler
+    console.error(err);
+    err = new ServerError(err);
+  }
+
+  return res.status(err.status).json(err.toResponseBody());
 }
 
 module.exports = {
